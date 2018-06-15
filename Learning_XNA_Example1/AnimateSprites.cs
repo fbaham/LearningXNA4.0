ab7b5696960580b8858ccb2bb539a223ebea3cd0
Francisco <francisco.bahamondes90@gmail.com>
2018-06-15 08:44:24 -0400

    First commit

---
+++ Learning_XNA_Example1/AnimateSprites.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Learning_XNA_Example1
+{
+	public class AnimateSprites : Sprite
+	{
+		#region Constructor
+		Point frameSize;
+		Point currentFrame;
+		Point sheetSize;
+
+		public AnimateSprites(Texture2D texture, Point frameSize, Point currentFrame, Point sheetSize)
+		{
+			this.texture = texture;
+			this.frameSize = frameSize;
+			this.currentFrame = currentFrame;
+			this.sheetSize = sheetSize;
+		}
+
+		#endregion
+
+		#region Propiedades
+
+		public Point FrameSize 
+		{ 
+			get { return frameSize; } 
+		}
+
+		public Point CurrentFrame
+		{
+			get { return currentFrame; }
+		}
+
+		public Point SheetSize
+		{
+			get { return sheetSize; }
+		}
+
+		public new Rectangle source
+		{
+			get { return new Rectangle(currentFrame.X * frameSize.X, currentFrame.Y * frameSize.Y, frameSize.X, frameSize.Y); }
+		}
+
+		#endregion
+
+		public void Update()
+		{
+			++currentFrame.X;
+			if (currentFrame.X >= sheetSize.X)
+			{
+				currentFrame.X = 0;
+				++currentFrame.Y;
+				if (currentFrame.Y >= sheetSize.Y)
+					currentFrame.Y = 0;
+			}
+		}
+	}
+}
---
+++ Learning_XNA_Example1/Sprite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Learning_XNA_Example1
+{
+	public class Sprite
+	{
+		/// Textura a dibujar
+		public Texture2D texture { get; set;}
+		/// Coordina desde la esquina superior izquierda la posición en la cual se dibujará la tectura
+		public Vector2 position { get; set;}
+		/// Permite dibujar solo una porción de la textura 
+		public Rectangle source { get; set;}
+		/// Color sobrepuesto a la textura, usa White para no sobreponer nada
+		public Color color { get; set; }
+		/// Rota la imagen
+		public float rotation { get; set; }
+		/// Indica el origen desde donde rotar la imagen
+		public Vector2 origin { get; set; }
+		/// Escala la textura
+		public float scale { get; set; }
+		/// Usa para voltear la imagen de forma horizontal o vertical
+		public SpriteEffects effects { get; set; }
+		/// Especifica qué imagen está por sobre la otra
+		public float layerDepth { get; set; }
+
+
+		//public void Draw(SpriteBatch spriteBatch)
+		//{
+		//	/// Llamada simple al metodo Draw
+		//	//spriteBatch.Draw(texture, position, color);
+
+		//	/// Llamada completa al metodo Draw
+		//	spriteBatch.Draw(texture, position, null, color, rotation, origin, scale, effects, layerDepth);
+
+		//}
+	}
+}
